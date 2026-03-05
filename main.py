2b95e19fff3ab7d6004a150f1b44e1f72764d7b4
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-05 13:08:34 +0545

    Added the feature of user choice input

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+machineOn = True
+
+while machineOn:
+    user_input = input("What would you like? (espresso/latte/cappuccino/):")
+    if user_input == 'off':
+        machinOn = False
+        break
